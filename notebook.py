f90e768c252508777ddc1384408e0b6ee917e0ce
malak khalil <malak.khalil@lau.edu>
2026-09-21 15:45:42 +0200

    test: add fibonacci test suite

--- notebook.py
+++ notebook.py
@@ -23,5 +23,28 @@ def fibonacci(n):
     pass
 
 
+@app.cell
+def _():
+    def test_fibonacci_zero():
+        assert fibonacci(0) == 0
+
+    def test_fibonacci_one():
+        assert fibonacci(1) == 1
+
+    def test_fibonacci_two():
+        assert fibonacci(2) == 1
+
+    def test_fibonacci_three():
+        assert fibonacci(3) == 2
+
+    def test_fibonacci_four():
+        assert fibonacci(4) == 3
+
+    def test_fibonacci_five():
+        assert fibonacci(5) == 5
+
+    return
+
+
 if __name__ == "__main__":
     app.run()
