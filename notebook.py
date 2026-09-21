f6ff03d879dc56712ad6b1901292c58b95da9614
malak khalil <malak.khalil@lau.edu>
2026-09-21 16:03:20 +0200

    refactor: simplify fibonacci base cases

--- notebook.py
+++ notebook.py
@@ -20,10 +20,8 @@ def _():
 
 @app.function
 def fibonacci(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n < 2:
+        return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
